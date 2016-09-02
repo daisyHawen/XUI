7074d7d3dd2a2d6ff6dc2de0c7af1c9e58891fea
daisyHawen <512309453@qq.com>
2016-09-02 16:46:28 +0800

    简化plus-minus.js

--- plus-minus-item/plus-minus.js
+++ plus-minus-item/plus-minus.js
@@ -2,15 +2,13 @@
  *专门用于添加元素，删除元素的函数
  *依赖jquery
  *输入参数
- *html:要插入元素的的html内容
  *fatherObj:要插入元素的父元素的名字，如果是class就用'.name'，如果是id就用'#name'
  *Obj:要插入元素的class类名,用'.className'
  *maxSize:允许插入的最大个数
  */
 
 var plusMinus = (function() {
-	var html = "",
-		fatherObj = "",
+	var fatherObj = "",
 		objClassName = "",
 		Counter = 1,
 		maxSize = 4;
@@ -19,6 +17,8 @@ var plusMinus = (function() {
 			if (Counter > maxSize) {
 				alert("只允许添加" + maxSize + "个条件");
 			} else {
+				var html = $(objClassName)[0].outerHTML;
+				console.log(html);
 				father_ul.append(html);
 				Counter = Counter + 1;
 			}
@@ -35,12 +35,10 @@ var plusMinus = (function() {
 				console.log('只剩一个了，不要再删了！');
 			}
 		},
-		init = function(ufatherObj, uobjClassName, uhtml, umaxSize) {
+		init = function(ufatherObj, uobjClassName, umaxSize) {
 			console.log('init');
 			fatherObj = ufatherObj;
 			objClassName = uobjClassName;
-			console.log(objClassName);
-			html = uhtml;
 			maxSize = umaxSize;
 		};
 	return {
@@ -50,21 +48,17 @@ var plusMinus = (function() {
 		minusItems: function() {
 			return minusItems();
 		},
-		init: function(ufatherObj, uobjClassName, uhtml, umaxSize) {
-			return init(ufatherObj, uobjClassName, uhtml, umaxSize);
+		init: function(ufatherObj, uobjClassName, umaxSize) {
+			return init(ufatherObj, uobjClassName, umaxSize);
 		}
 	}
 
 });
 var newplusMinus = plusMinus();
-var html = "<li class='list-group-item'> <div class='input-group'> <div class='input-group-btn'> <button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>纬度范围 <span class='caret'></span></button> <ul class='dropdown-menu'> <li><a href='#'>Action</a></li> <li><a href='#'>Another action</a></li> <li><a href='#'>Something else here</a></li> </ul> </div> <!-- /btn-group --> <input type='text' class='form-control' style='width:95%' aria-label='...'> <div class='plus-minus'> <a onclick = 'addItem()'><span class='glyphicon glyphicon-plus'></span> </a> <a onclick='minusItem()'><span class='glyphicon glyphicon-minus'></span> </a> </div> </div> </li> ";
-var html2 = $('.list-group-item').last().html();
-console.log(html2);
-console.log(html);
 var limit_ul = '.list-group';
 var limit_li = '.list-group-item';
 /*初始化参数*/
-newplusMinus.init(limit_ul, limit_li, html, 5);
+newplusMinus.init(limit_ul, limit_li, 5);
 /*为按钮绑定事件函数*/
 function addItem() {
 	newplusMinus.plusItems();
